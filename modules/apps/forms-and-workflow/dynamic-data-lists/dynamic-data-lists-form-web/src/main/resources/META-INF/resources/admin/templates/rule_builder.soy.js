cb1c6024ec0585324264aa87db50775c7a771c54
Thiago Rocha <thiago.rocha@liferay.com>
2017-03-23 15:48:18 -0300

    LPS-70564 BuildSoy

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/templates/rule_builder.soy.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/templates/rule_builder.soy.js
@@ -21,25 +21,25 @@ ddl.rule_list = function(opt_data, opt_ignored) {
   var output = '';
   if (opt_data.rules.length > 0) {
     output += '<ul class="ddl-form-body-content form-builder-rule-builder-rules-list tabular-list-group">';
-    var ruleList219 = opt_data.rules;
-    var ruleListLen219 = ruleList219.length;
-    for (var ruleIndex219 = 0; ruleIndex219 < ruleListLen219; ruleIndex219++) {
-      var ruleData219 = ruleList219[ruleIndex219];
+    var ruleList234 = opt_data.rules;
+    var ruleListLen234 = ruleList234.length;
+    for (var ruleIndex234 = 0; ruleIndex234 < ruleListLen234; ruleIndex234++) {
+      var ruleData234 = ruleList234[ruleIndex234];
       output += '<li class="list-group-item"><div class="clamp-horizontal list-group-item-content"><p class="form-builder-rule-builder-rule-description text-default"><b>If </b>';
-      var conditionList197 = ruleData219.conditions;
-      var conditionListLen197 = conditionList197.length;
-      for (var conditionIndex197 = 0; conditionIndex197 < conditionListLen197; conditionIndex197++) {
-        var conditionData197 = conditionList197[conditionIndex197];
-        output += ddl.condition({operandType: conditionData197.operands[0].type, operandValue: conditionData197.operands[0].label}) + '<b class="text-lowercase"><em> ' + soy.$$escapeHtml(opt_data.strings[conditionData197.operator]) + ' </em></b>' + ((conditionData197.operands[1]) ? ddl.condition({operandType: conditionData197.operands[1].type, operandValue: conditionData197.operands[1].label != null ? conditionData197.operands[1].label : conditionData197.operands[1].value}) : '') + ((! (conditionIndex197 == conditionListLen197 - 1)) ? '<br /><b> ' + soy.$$escapeHtml(conditionData197.logicOperator) + ' </b>' : '');
+      var conditionList212 = ruleData234.conditions;
+      var conditionListLen212 = conditionList212.length;
+      for (var conditionIndex212 = 0; conditionIndex212 < conditionListLen212; conditionIndex212++) {
+        var conditionData212 = conditionList212[conditionIndex212];
+        output += ddl.condition({operandType: conditionData212.operands[0].type, operandValue: conditionData212.operands[0].label}) + '<b class="text-lowercase"><em> ' + soy.$$escapeHtml(opt_data.strings[conditionData212.operator]) + ' </em></b>' + ((conditionData212.operands[1]) ? ddl.condition({operandType: conditionData212.operands[1].type, operandValue: conditionData212.operands[1].label != null ? conditionData212.operands[1].label : conditionData212.operands[1].value}) : '') + ((! (conditionIndex212 == conditionListLen212 - 1)) ? '<br /><b> ' + soy.$$escapeHtml(ruleData234.logicOperator) + ' </b>' : '');
       }
       output += '<br />';
-      var actionList205 = ruleData219.actions;
-      var actionListLen205 = actionList205.length;
-      for (var actionIndex205 = 0; actionIndex205 < actionListLen205; actionIndex205++) {
-        var actionData205 = actionList205[actionIndex205];
-        output += ddl.action({action: actionData205}) + ((! (actionIndex205 == actionListLen205 - 1)) ? ', <br /><b> and </b>' : '');
+      var actionList220 = ruleData234.actions;
+      var actionListLen220 = actionList220.length;
+      for (var actionIndex220 = 0; actionIndex220 < actionListLen220; actionIndex220++) {
+        var actionData220 = actionList220[actionIndex220];
+        output += ddl.action({action: actionData220}) + ((! (actionIndex220 == actionListLen220 - 1)) ? ', <br /><b> and </b>' : '');
       }
-      output += '</p></div><div class="list-group-item-field"><div class="card-col-field"><div class="dropdown"><a class="dropdown-toggle icon-monospaced" data-toggle="dropdown" href="#1">' + soy.$$filterNoAutoescape(opt_data.kebab) + '</a><ul class="dropdown-menu dropdown-menu-right"><li class="rule-card-edit" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex219) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings.edit) + '</a></li><li class="rule-card-delete" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex219) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings['delete']) + '</a></li></ul></div></div></div></li>';
+      output += '</p></div><div class="list-group-item-field"><div class="card-col-field"><div class="dropdown"><ul class="dropdown-menu dropdown-menu-right"><li class="rule-card-edit" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex234) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings.edit) + '</a></li><li class="rule-card-delete" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex234) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings['delete']) + '</a></li></ul><a class="dropdown-toggle icon-monospaced" data-toggle="dropdown" href="#1">' + soy.$$filterNoAutoescape(opt_data.kebab) + '</a></div></div></div></li>';
     }
     output += '</ul>';
   } else {
